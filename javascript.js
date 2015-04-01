54c4118e139999262576aa349880ab13c6f96992
DouglasKGlover <douglas.glover@outlook.com>
2015-04-01 00:37:27 -0400

    April fool's!

--- javascript.js
+++ javascript.js
@@ -91,4 +91,50 @@ $(document).ready(function(){
         });
     }
 
+
+
+
+    /*
+    * Fun shit for no good reason
+    * */
+
+    // Upside-down avatars
+    $('img[src*="avatar"]').css("transform", "rotate(180deg)");
+
+    // tuffmuff raindbow
+    $("a.bigusername span:contains('tuffmuff')").html("" +
+    "<span style='color:#f13c9d'>t</span>" +
+    "<span style='color:#f80b25'>u</span>" +
+    "<span style='color:#f78121'>f</span>" +
+    "<span style='color:#f9e81f'>f</span>" +
+    "<span style='color:#1e9730'>m</span>" +
+    "<span style='color:#1b86bc'>u</span>" +
+    "<span style='color:#243796'>f</span>" +
+    "<span style='color:#6f0a82'>f</span>" +
+    "");
+
+    // Tell everyone how cool i am
+    var noidArray = [
+        "Noid rocks!",
+        "I can't help but swoon over that Noid guy!",
+        "Man, have you SEEN Noid's SIX PACK!?",
+        "Noid really melts my butter!",
+        "Noid's so cool!",
+        "I'd let Noid steal MY pizza!",
+        "Noid sure knows how to treat a lady!",
+        "A little off-topic, but has anybody else noticed how AWESOME Noid is?",
+        "I like turtles.",
+        "Release the Kraken!",
+        "She sells sea shells by the sea shore.",
+        "Battleship was a really underrated game.",
+        "Magus for GOTY 2014!",
+        "Back to the OT, did you guys see Noid's cool suit? I want one!",
+        "My ankles smell like almonds..."
+    ];
+    $('div[id^=post_message]').each(function(){
+        $(this).prepend("<span style='color: red;'>"+ noidArray[random = Math.ceil(Math.random() * 15)-1] +"</span><br/><br/>");
+        console.log(random = Math.ceil(Math.random() * 5)-1);
+    });
+
+
 });
