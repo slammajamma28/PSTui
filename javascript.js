f22bf77489ef1cf737a18cc22dac3ec2a2b25e27
slammajamma28 <slammajamma28@gmail.com>
2025-01-08 15:08:14 -0500

    More changes

--- javascript.js
+++ javascript.js
@@ -2,18 +2,38 @@ $(document).ready(function(){
 
     console.log("PSTui Loaded");
 
-    // Move Save Game Info box so that I don't need to scroll when updating stack info
     var currentPath = window.location.href;
+    // Make tweaks to edit game page
     if (currentPath.includes("admtools/game_edit")) {
+        
+        // Enable the enter button to save changes
+        $(document).keypress(function(){
+            if(e.which == 13) {
+                $("#game_edit_btn button").click()
+            }
+        })
+
+        // Move Save Game Info box so that I don't need to scroll when updating stack info
         var edit_form = $("#game_edit .section:eq(0)");
-        var submit_changes = $(".col-md-8 section:eq(5)");
+        var submit_changes = $(".col-md-8 section:eq(7)");
         submit_changes.appendTo(edit_form);
+
     }
+
     // Make tweaks to forum creation page
     if (currentPath.includes("forum/admin")){
+
+        // Enable the enter button to save changes
+        $(document).keypress(function(){
+            if(e.which == 13) {
+                $("button.ipsButton_primary").click()
+            }
+        })
+
         if (currentPath.includes("do=form")){
+        
             // Auto-check Discussions option
-            document.getElementById("elRadio_forum_type_normal_").click();
+            document.getElementById("elRadio_forum_type_normal_forum_type").click();
 
             // Remove fields I don't use
             $("#form_new_header_permissions").css({"display": "none"})
@@ -28,16 +48,23 @@ $(document).ready(function(){
             $("#forum_password_on").css({"display": "none"})
             $("#forum_skin_id").css({"display": "none"})
             $("#forum_ipseo_priority").css({"display": "none"})
+            $("#form_new_forum_type li:eq(1)").css({"display": "none"})
 
             // Collapse Parent Forum options menu
-            $("#form_new_forum_parent_id .ipsFieldRow_content .ipsSelectTree .ipsSelectTree_nodes .ipsScrollbar ul li .ipsSelectTree_itemOpen").removeClass("ipsSelectTree_itemOpen")
-            $("#form_new_forum_parent_id .ipsFieldRow_content .ipsSelectTree .ipsSelectTree_nodes .ipsScrollbar ul li div[data-role]").css({"style":"display: none"}).css({"display": "none"})
+            $("div.acpFormTabContent .ipsFieldRow_content .ipsSelectTree .ipsSelectTree_nodes .ipsScrollbar ul li .ipsSelectTree_itemOpen").removeClass("ipsSelectTree_itemOpen")
+            $("div.acpFormTabContent .ipsFieldRow_content .ipsSelectTree .ipsSelectTree_nodes .ipsScrollbar ul li div[data-role]").css({"style":"display: none"}).css({"display": "none"})
+
         }
+
         if (currentPath.includes("do=permissions")) {
-            // Hide table, I don't wanna change any permissions
-            $("table.ipsTable").css({"display": "none"})
+
+            // Hide some rows so that the Submit button is in line with the previous page
+            $("tbody tr:eq(0)").css({"display": "none"})
+            $("tbody tr:eq(1)").css({"display": "none"})
+            $("tbody tr:eq(2)").css({"display": "none"})
+            $("tbody tr:eq(3)").css({"display": "none"})
+            $("tbody tr:eq(4)").css({"display": "none"})
+
         }
     }
-        
-        $("table.ipsTable")
 });
